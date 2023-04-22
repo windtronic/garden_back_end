14a037bf034cbf0ae65e120b0e278e3e0541db1a
Toni <windtronic89@gmail.com>
2023-04-22 14:46:55 -0600

    trying deployment

--- garden_project/garden_django/settings.py
+++ garden_project/garden_django/settings.py
@@ -11,10 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+import os
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
@@ -100,13 +100,7 @@ WSGI_APPLICATION = 'garden_django.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'garden',
-        'USER': 'gardenuser',
-        'PASSWORD': 'garden',
-        'HOST': 'localhost'
-    }
+    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 
@@ -145,6 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- garden_project/garden_django/urls.py
+++ garden_project/garden_django/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.conf.urls import include
 from django.urls import path
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +26,4 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls'))
 ]
 
+urlpatterns += staticfiles_urlpatterns()
