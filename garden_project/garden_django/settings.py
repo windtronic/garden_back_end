ff40e8205e457975faa79752cbbbbee7949399fe
Toni <windtronic89@gmail.com>
2023-04-22 17:02:43 -0600

    updates

--- garden_project/garden_django/settings.py
+++ garden_project/garden_django/settings.py
@@ -100,7 +100,7 @@ WSGI_APPLICATION = 'garden_django.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
