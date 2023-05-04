452b0516e822e33b390aefc3a5b6b7a464af78ab
Toni <windtronic89@gmail.com>
2023-05-04 09:50:13 -0600

    starting deployment again

--- garden_project/garden_django/urls.py
+++ garden_project/garden_django/urls.py
@@ -17,7 +17,7 @@ Including another URLconf
 from django.conf.urls import include
 from django.urls import path
 from django.contrib import admin
-from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -26,5 +26,3 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     
 ]
-
-urlpatterns += staticfiles_urlpatterns()
