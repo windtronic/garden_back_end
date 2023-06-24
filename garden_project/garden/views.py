fa5ae4767818c752cd68ed18d01bb21ba48f4499
Toni <windtronic89@gmail.com>
2023-06-24 10:17:14 -0600

    auth

--- garden_project/garden/views.py
+++ garden_project/garden/views.py
@@ -1,14 +1,17 @@
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, authenticate, login
 from django.http import JsonResponse
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 from rest_framework import generics
 from django.views.decorators.csrf import csrf_exempt
-from django.contrib.auth import authenticate, login
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-from .serializers import UserSerializer, CalendarSerializer, PlantSerializer, PlantListingSerializer
+from .serializers import (
+    UserSerializer,
+    CalendarSerializer,
+    PlantSerializer,
+    PlantListingSerializer
+)
 from .models import User, Calendar, Plant, PlantListing
-from rest_framework.permissions import AllowAny
-
 
 
 class UserList(generics.ListCreateAPIView):
@@ -21,29 +24,36 @@ class UserList(generics.ListCreateAPIView):
             print(user.name)
         return queryset
 
+
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
+
+
 class UserCreate(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
 
 class UserUpdate(generics.UpdateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
+
 class UserDelete(generics.DestroyAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
 
 class CalendarList(generics.ListCreateAPIView):
     queryset = Calendar.objects.all()
     serializer_class = CalendarSerializer
 
+
 class CalendarDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Calendar.objects.all()
     serializer_class = CalendarSerializer
+
 
 class PlantList(generics.ListCreateAPIView):
     queryset = Plant.objects.order_by('name')
@@ -54,33 +64,43 @@ class PlantDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Plant.objects.all()
     serializer_class = PlantSerializer
 
+
 class PlantCreate(generics.CreateAPIView):
     queryset = Plant.objects.all()
     serializer_class = PlantSerializer
+
 
 class PlantUpdate(generics.UpdateAPIView):
     queryset = Plant.objects.all()
     serializer_class = PlantSerializer
 
+
 class PlantDelete(generics.DestroyAPIView):
     queryset = Plant.objects.all()
     serializer_class = PlantSerializer
+
 
 class PlantListingList(generics.ListCreateAPIView):
     queryset = PlantListing.objects.all()
     serializer_class = PlantListingSerializer
 
+
 class PlantListingDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = PlantListing.objects.all()
     serializer_class = PlantListingSerializer
 
+
+@api_view(['POST'])
+@csrf_exempt
+@permission_classes([AllowAny])
 def register_view(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        email = request.POST.get('email')
-        address = request.POST.get('address')
-        user = User.objects.create_user(username, email, password)
+        username = request.data.get('username')
+        password = request.data.get('password')
+        email = request.data.get('email')
+        address = request.data.get('address')
+        User = get_user_model()
+        user = User.objects.create_user(username=username, email=email, password=password)
         user.address = address
         user.save()
         return JsonResponse({'success': True})
@@ -88,9 +108,9 @@ def register_view(request):
         return JsonResponse({'success': False})
 
 
-
-@csrf_exempt
 @api_view(['POST'])
+@csrf_exempt
+@permission_classes([AllowAny])
 def login_view(request):
     email = request.data.get('email')
     password = request.data.get('password')
